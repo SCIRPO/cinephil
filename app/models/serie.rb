8cf6a56a331b394911ccc64a36751e288237e558
Cheikhou Diakhaby <diakhaby_cheik@yahoo.fr>
2021-02-04 20:57:23 +0100

    LIKE is ok

--- app/models/serie.rb
+++ app/models/serie.rb
@@ -1,5 +1,6 @@
 class Serie < ApplicationRecord
     has_many :seasons, dependent: :destroy
     has_many :episodes, through: :seasons
+    has_many :likes, dependent: :destroy
     has_one_attached :photo
 end
