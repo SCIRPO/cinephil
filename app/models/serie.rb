da097da7920878b2ba494533a9ca53aeb5888ae0
sebastien scirpo <sebastien.scirpo@gmail.com>
2021-02-18 21:53:36 +0100

    improved

--- app/models/serie.rb
+++ app/models/serie.rb
@@ -1,8 +1,16 @@
 class Serie < ApplicationRecord
-    has_many :seasons, dependent: :destroy
-    has_many :episodes, through: :seasons
-    has_many :likes, dependent: :destroy
-    has_one_attached :photo
-    GENRELIST = ["Action & Adventure", "Crime", "Drama", "Mystery", "Sci-Fi & Fantasy", "War & Politics"]
-    PLATFORMLIST = ["Amazon Prime", "BBC One", "Disney+", "HBO", "Netflix",]
+  has_many :seasons, dependent: :destroy
+  has_many :episodes, through: :seasons
+  has_many :likes, dependent: :destroy
+  has_one_attached :photo
+  GENRELIST = ["Action & Adventure", "Crime", "Drama", "Mystery", "Sci-Fi & Fantasy", "War & Politics"]
+  PLATFORMLIST = ["Amazon Prime", "BBC One", "Disney+", "HBO", "Netflix",]
+
+  def episode_count
+    counter = 0
+    seasons.each do |season|
+      counter += season.episodes.count
+    end
+    counter
+  end
 end
