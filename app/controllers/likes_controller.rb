9f2de28d69a8292205dafffd93257f5514897cb6
sebastien scirpo <sebastien.scirpo@gmail.com>
2021-02-20 17:21:59 +0100

    MarquePage_Whislist done

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,11 +4,24 @@ class LikesController < ApplicationController
     unless Like.find_by(user: current_user, serie: @serie)
       Like.create(user: current_user, serie: @serie)
     end
-    redirect_to series_path(@serie)
+    if request.referer.match? /\Wwhishlist/
+      redirect_to whishlist_path
+    elsif request.referer.match? /\Wseries\/[0-9].*/
+      redirect_to series_path(@serie)
+    else
+      redirect_to series_index_path
+    end
   end
 
   def destroy
+    @serie = Like.find(params[:id]).serie
     Like.find(params[:id]).destroy
-    redirect_to whishlist_path
+    if request.referer.match? /\Wwhishlist/
+      redirect_to whishlist_path
+    elsif request.referer.match? /\Wseries\/[0-9].*/
+      redirect_to series_path(@serie)
+    else
+      redirect_to series_index_path
+    end
   end
 end
