a0506103342b4b112f25687c545e59692a27cb5d
Cheikhou Diakhaby <diakhaby_cheik@yahoo.fr>
2021-02-27 13:00:14 +0100

    fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,26 @@ class PagesController < ApplicationController
   end
 
   def whishlist
-    @likes = current_user.likes.where(viewed:false)
+    @all_likes = current_user.likes
+    @viewed_likes =  current_user.likes
+                                 .joins(user: [ :viewings ])
+                                 .where(viewings: {viewable_type: "Serie"})
+                                 .where("viewings.viewable_id = likes.serie_id")
+    
+    # Equivalent en SQL :
+    # Tous les séries qui sont liked ET viewed
+    #   SELECT COUNT(*) FROM "likes"
+    #   INNER JOIN "users" ON "users"."id" = "likes"."user_id"
+    #   INNER JOIN "viewings" ON "viewings"."user_id" = "users"."id"
+    #   WHERE "likes"."user_id" = current_user.id
+    #   AND "viewings"."viewable_type" = "Serie"
+    #   AND (viewings.viewable_id = likes.serie_id)
+
+    @likes = @all_likes - @viewed_likes
   end
 
   def view
-    @views = current_user.likes.where(viewed:true)
+    @views = current_user.likes
   end
 
   def library
